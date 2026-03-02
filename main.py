b8b8d2766539c1ea5a759df28552abfcbf93bbde
Patrick-Schuck <p.schuck-castle@t-online.de>
2026-03-02 12:54:12 +0100

    Added get file content functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_functions import available_functions
  
 def main():
     print("Hello from ai-agent!")
@@ -24,13 +25,17 @@ def main():
     response = client.models.generate_content(
         model ="gemini-2.5-flash",
         contents = messages,
-        config = types.GenerateContentConfig(system_instruction = system_prompt, temperature=0))
+        config = types.GenerateContentConfig(tools=[available_functions],system_instruction = system_prompt))
     if response.usage_metadata:
         if args.verbose:
             print(f"User prompt: {args.user_prompt}")
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    if response.function_calls:
+        for func_call in response.function_calls:
+            print(f"Calling function: {func_call.name}({func_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
