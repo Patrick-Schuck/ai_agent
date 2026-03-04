37815df719bcfaecbebf17478055c177c0d9bb63
Patrick-Schuck <p.schuck-castle@t-online.de>
2026-03-04 17:19:22 +0100

    added requesting loop.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import argparse
 from dotenv import load_dotenv
@@ -7,8 +8,6 @@ from prompts import system_prompt
 from call_functions import available_functions, call_function
  
 def main():
-    print("Hello from ai-agent!")
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
@@ -22,31 +21,42 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model ="gemini-2.5-flash",
-        contents = messages,
-        config = types.GenerateContentConfig(tools=[available_functions],system_instruction = system_prompt))
-    if response.usage_metadata:
-        if args.verbose:
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if response.function_calls:
-        response_list = []
-        for func_call in response.function_calls:
-            function_call_result=call_function(func_call)
-            if not function_call_result.parts:
-                raise Exception
-            if not function_call_result.parts[0].function_response:
-                raise Exception
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception
-            response_list.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
 
+    for run in range(20):
+        print(run)
+        response = client.models.generate_content(
+            model ="gemini-2.5-flash",
+            contents = messages,
+            config = types.GenerateContentConfig(tools=[available_functions],system_instruction = system_prompt))
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        if response.usage_metadata:
+            if args.verbose:
+                print(f"User prompt: {args.user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            response_list = []
+            for func_call in response.function_calls:
+                function_call_result=call_function(func_call)
+                if not function_call_result.parts:
+                    raise Exception
+                if not function_call_result.parts[0].function_response:
+                    raise Exception
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception
+                response_list.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(response.text)
+            return True
+
+        messages.append(types.Content(role="user", parts=response_list))
+
+    print("The model went through too many loops! Too complex task ...")
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
